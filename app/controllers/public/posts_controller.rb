0a0b1664502d059a6208694302335d66bfd288aa
yk-sora <yktn5mx@gmail.com>
2024-07-25 09:42:06 +0000

    post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,31 @@
 class Public::PostsController < ApplicationController
+  
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image, notice: "画像が投稿されました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
   def index
     @posts = Post.all
   end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :description, :image_data)
+  end
+
+  
 end
